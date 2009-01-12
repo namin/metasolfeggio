81fcda4a7843c71f11ebbfb4b4c3e4d479f97504
Nada Amin <namin@alum.mit.edu>
2009-01-12 12:56:49 +0100

    getting started on variations of Simple Gift

---
+++ mingus/shakers.py
@@ -0,0 +1,15 @@
+doc = """
+    Simple Gifts
+ an 1848 Shaker song
+
+'Tis the gift to be simple, 'tis the gift to be free,
+'Tis the gift to come down where you ought to be,
+And when we find ourselves in the place just right,
+'Twill be in the valley of love and delight.
+When true simplicity is gain'd,
+To bow and to bend we shan't be asham'd,
+To turn, turn will be our delight,
+Till by turning, turning we come round right.
+"""
+
+

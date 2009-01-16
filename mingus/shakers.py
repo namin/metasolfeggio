13968abac3027d74a4a4f482f8365ae75d44cb83
Nada Amin <namin@alum.mit.edu>
2009-01-16 09:50:32 +0100

    shakers' simple gifts melody from wikipedia

--- mingus/shakers.py
+++ mingus/shakers.py
@@ -1,3 +1,8 @@
+from mingus.containers.Bar import Bar
+from mingus.containers.Track import Track
+from mingus.midi import fluidsynth
+import play; play.init()
+
 doc = """
     Simple Gifts
  an 1848 Shaker song
@@ -12,4 +17,39 @@ To turn, turn will be our delight,
 Till by turning, turning we come round right.
 """
 
+def toTrack(key, meter, melody):
+    _, base = meter
+    t = Track()
+    for mb in melody:
+        b = Bar(key, meter)
+        for mn in mb.split():
+            nr = mn.split(':')
+            n = nr[0]
+            r = base
+            if len(nr) > 1:
+                r = int(nr[1])
+            ok = b.place_notes(n, r)
+        t.add_bar(b)
+    return t
 
+melody = [                        'G-4         G-4',
+          'C-5         C-5:8 D-5:8 E-5:8 C-5:8 E-5:8 F-5:8',
+          'G-5         G-5:8 G-5:8 E-5         D-5:8 C-5:8',
+          'D-5         D-5         D-5         D-5'        ,
+          'D-5:8 E-5:8 D-5:8 B-4:8 G-4         G-4'        ,
+          'C-5:8 B-4:8 C-5:8 D-5:8 E-5         D-5:8 D-5:8',
+          'E-5         F-5         G-5:3             G-5:8',
+          'D-5         D-5:8 E-5:8 D-5         C-5:8 C-5:8',
+          'D-5         C-5:8 B-4:8 C-5:2'                  ,
+          'G-5:2             E-5:3                   D-5:8',
+          'E-5:8 F-5:8 E-5:8 D-5:8 C-5:3             D-5:8',
+          'E-5         E-5:8 F-5:8 G-5         E-5'        , 
+          'D-5         D-5:8 E-5:8 D-5:3             G-4:8',
+          'C-5:2                   C-5:3             D-5:8',
+          'E-5         E-5:8 F-5:8 G-5         G-5:8 G-5:8',
+          'D-5         D-5         E-5         E-5:8 D-5:8',
+          'C-5         C-5         C-5:2']
+
+track = toTrack("C", (4,4), melody)
+
+fluidsynth.play_Track(track)

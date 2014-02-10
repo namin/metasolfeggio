336867e92e40038196d7f6a3ccb5673e7d310c19
Nada Amin <namin@alum.mit.edu>
2014-02-10 18:31:15 +0100

    update project settings

--- overtone/project.clj
+++ overtone/project.clj
@@ -1,6 +1,6 @@
 (defproject metasolfeggio "0.1-SNAPSHOT"
   :description "Computer-Aided Harmony and Counterpoint"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.3"]
-                 [overtone "0.8.1"]
-                 [leipzig "0.4.0-SNAPSHOT"]])
+                 [org.clojure/core.logic "0.8.7"]
+                 [overtone "0.9.1"]
+                 [leipzig "0.7.0"]])

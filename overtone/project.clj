8b531214f455633e4250ab635d12f0a3d21b8c19
Nada Amin <namin@alum.mit.edu>
2014-02-10 19:23:48 +0100

    add core.match dep

--- overtone/project.clj
+++ overtone/project.clj
@@ -2,5 +2,6 @@
   :description "Computer-Aided Harmony and Counterpoint"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.8.7"]
+                 [org.clojure/core.match "0.2.1"]
                  [overtone "0.9.1"]
                  [leipzig "0.7.0"]])

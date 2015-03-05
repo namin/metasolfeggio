2f65e8e749fd3a500cf2b842b6cbde728078ba1f
Nada Amin <nada.amin@epfl.ch>
2015-03-05 10:12:22 +0100

    update all deps but leipzig

--- overtone/project.clj
+++ overtone/project.clj
@@ -1,10 +1,11 @@
 ;; add to ~/.lein/profiles.clj the following line
-;; {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}}}
+;; {:user {:plugins [[cider/cider-nrepl "0.8.1"]]}}}}
 
 (defproject metasolfeggio "0.1-SNAPSHOT"
   :description "Computer-Aided Harmony and Counterpoint"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.7"]
-                 [org.clojure/core.match "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.8"]
+                 [org.clojure/core.match "0.2.2"]
                  [overtone "0.9.1"]
-                 [leipzig "0.7.0" :exclusions [org.clojure/clojure]]])
+                 [leipzig "0.7.0" :exclusions [org.clojure/clojure]] ;; not updated to 0.8.0
+                 ])

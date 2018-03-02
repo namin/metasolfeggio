c69284b49df597e7abd01de68adf98f33f852f0e
Nada Amin <na482@cl.cam.ac.uk>
2018-03-02 23:10:17 +0000

    my first sonic pi program cc @samaaron

---
+++ sonic-pi/iam.rb
@@ -0,0 +1,45 @@
+# background tune
+# IAM - Un cri court dans la nuit feat. Daddy Nuttea
+# https://www.youtube.com/watch?v=7pjLV2-pnbY (Audio officiel)
+#
+t = 0.7
+
+loop do
+  
+  play 72
+  sleep t
+  play 75
+  sleep t
+  play 79
+  sleep t
+  play 75
+  sleep t
+  
+  play 71
+  sleep t
+  play 75
+  sleep t
+  play 79
+  sleep t
+  play 75
+  sleep t
+  
+  play 70
+  sleep t
+  play 75
+  sleep t
+  play 79
+  sleep t
+  play 75
+  sleep t
+  
+  play 69
+  sleep t
+  play 75
+  sleep t
+  play 77
+  sleep t
+  play 75
+  sleep t
+  
+end

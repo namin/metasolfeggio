30f42ffd922edf3e3475c7aca264ab7b4d5402dc
Nada Amin <na482@cl.cam.ac.uk>
2018-03-04 11:55:19 +0000

    one more link

--- sonic-pi/unlock0.rb
+++ sonic-pi/unlock0.rb
@@ -1,5 +1,6 @@
 # unlocking Shakers' hymn aka Simple gifts
 # https://www.youtube.com/watch?v=CLAnuG1340g
+# https://www.youtube.com/watch?v=eOx9quc-iow
 # with the Lord of the Dance
 # https://www.youtube.com/watch?v=xhs9ZNH9dHE
 # https://www.youtube.com/watch?v=gtM8BRF7Bd0

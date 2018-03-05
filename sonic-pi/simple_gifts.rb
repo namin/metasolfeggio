60e66496f1d9feafe240f835263bc96438bc556a
Nada Amin <namin@alum.mit.edu>
2018-03-05 19:27:48 +0000

    u

--- sonic-pi/simple_gifts.rb
+++ sonic-pi/simple_gifts.rb
@@ -1,8 +1,8 @@
 use_bpm 120
 
 #       ;;;  it is a gift to be simple
-#       (phrase [ 1   1   1 1/2 1/2 1/2 1/2]
-#               [-3  -3   0   0   1   2   0])
+#       (phrase [ 1    1   1  1/2 1/2 1/2 1/2]
+#               [-sol -sol do do  re  mi  do])
 
 play 67
 sleep 1
@@ -20,9 +20,8 @@ play 72
 sleep 0.6
 
 #       ;;; it's a  gift to be free
-#       (then (phrase [1/2 1/2   1 1/2 1/2   1]
-#                     [  3   4   5   5   4   3]))
-#       TODO leipzig was wrong
+#       (then (phrase [1/2 1/2  1   1/2 1/2   1]
+#                     [mi  fa   sol sol sol   mi]))
 play 76
 sleep 0.4
 play 77
@@ -31,14 +30,38 @@ play 79
 sleep 1
 play 79
 sleep 0.5
-play 77
+play 79
 sleep 0.5
 play 76
 sleep 1
 
 #       ;;; it's a gift to come round where we ought to be
-#       (then (phrase [1/2 1/2   1   1   1   1 1/2 1/2 1/2 1/2   1]
-#                     [  1   0   1   1   1   1   1   2   1  -1  -3]))
+#       (then (phrase [1/2 1/2  1   1   1   1   1/2 1/2 1/2 1/2   1]
+#                     [re  do   re  re  re  re  re  mi  re  -si  -sol]))
+
+play 74
+sleep 0.5
+play 72
+sleep 0.5
+play 74
+sleep 1
+play 74
+sleep 1
+play 74
+sleep 1
+play 74
+sleep 1
+play 74
+sleep 0.5
+play 76
+sleep 0.5
+play 74
+sleep 0.5
+play 71
+sleep 0.5
+play 67
+sleep 1
+
 #       ;;; and when we find ourselves in the place just right
 #       (then (phrase [  1 1/2 1/2 1/2 1/2   1 1/2 1/2   1   1]
 #                     [ -3   0  -1   0   1   2   1   1   2   3]))

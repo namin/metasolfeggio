d7c51bb549dc1dbbe051d8af0e83e692208058ca
Nada Amin <namin@alum.mit.edu>
2018-03-05 18:14:28 +0000

    lyrics of beginning

---
+++ sonic-pi/lord_of_the_dance.rb
@@ -0,0 +1,29 @@
+# Lord of the Dance
+# https://www.youtube.com/watch?v=xhs9ZNH9dHE
+#
+
+# I danced in the morning
+# when the world was begun
+#
+# I danced in the Moon
+# and the Stars and the Sun
+#
+#
+# And I came down from Heaven
+# and I danced on Earth
+#
+# At Bethlehem I had my birth
+#
+#
+# Dance then,
+# wherever you may be,
+#
+# I am the Lord
+# of the Dance, said He!
+#
+#
+# And I'll lead you all
+# wherever you may be,
+# 
+# and I'll lead you all
+# in the Dance, said He!

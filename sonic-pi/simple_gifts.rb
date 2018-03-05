7b242c81aef7fd77d79a0d2e2eefb36d76d83542
Nada Amin <namin@alum.mit.edu>
2018-03-05 20:23:25 +0000

    formatting

--- sonic-pi/simple_gifts.rb
+++ sonic-pi/simple_gifts.rb
@@ -19,7 +19,7 @@ sleep 0.5
 play 72
 sleep 0.6
 
-#       ;;; it's a  gift to be free
+#       ;;; 'Tis a  gift to be free
 #       (then (phrase [1/2 1/2  1   1/2 1/2   1]
 #                     [mi  fa   sol sol sol   mi]))
 play 76
@@ -35,7 +35,7 @@ sleep 0.5
 play 76
 sleep 1
 
-#       ;;; it's a gift to come round where we ought to be
+#       ;;; 'Tis a gift to come round where we ought to be
 #       (then (phrase [1/2 1/2  1   1   1   1   1/2 1/2 1/2 1/2   1]
 #                     [re  do   re  re  re  re  re  mi  re  -si  -sol]))
 
@@ -65,18 +65,23 @@ sleep 1
 #       ;;; and when we find ourselves in the place just right
 #       (then (phrase [  1 1/2 1/2 1/2 1/2   1 1/2 1/2   1   1]
 #                     [ -3   0  -1   0   1   2   1   1   2   3]))
+
 #       ;;; it will be in the valley of love and delight
 #       (then (phrase [3/2 1/2   1 1/2 1/2   1 1/2 1/2   1 1/2 1/2  2]
 #                     [  4   4   1   1   2   1   0   0   1   0  -1  0]))
+
 #       ;;; when true simplicity is gained
 #       (then (phrase [  2 3/2 1/2 1/2 1/2 1/2 1/2 3/2]
 #                     [  4   2   1   2   3   2   1   0]))
+
 #       ;;; to bow and to bend we shan't be ashamed
 #       (then (phrase [1/2   1 1/2 1/2   1   1   1 1/2 1/2 3/2]
 #                     [  1   2   2   3   4   2   1   1   2   1]))
+
 #       ;;; to turn, turn will be our delight
 #       (then (phrase [1/2   2 3/2 1/2   1 1/2 1/2   1]
 #                     [ -3   0   0   1   2   2   3   4]))
+
 #       ;;; till by turning, turning we come round right
 #       (then (phrase [1/2 1/2   1   1   1 1/2 1/2   1   1   1]
 #                     [  4   4   1   1   2   2   1   0   0   0]))

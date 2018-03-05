ddf069ef98a58cb5627745b396dccc29fd207668
Nada Amin <namin@alum.mit.edu>
2018-03-05 20:46:32 +0000

    from scratch

--- sonic-pi/simple_gifts.rb
+++ sonic-pi/simple_gifts.rb
@@ -1,7 +1,36 @@
 # Recited from
 # https://www.youtube.com/watch?v=CLAnuG1340g
 
+use_bpm 80
+
+__sol = 65
+_do   = 72
+_re   = 74
+_mi   = 76
+
 # 'Tis a gift to be simple,
+# 'Tis
+play __sol
+sleep 0.5
+# a
+play __sol
+sleep 0.5
+# gift
+play _do
+sleep 1
+# to
+play _do
+sleep 0.5
+# be
+play _re
+sleep 0.5
+# sim-
+play _mi
+sleep 0.5
+# -ple
+play _do
+sleep 0.5
+
 
 # 'Tis a gift to be free.
 
@@ -28,91 +57,3 @@
 # Till by turning, turning
 
 # We come 'round right.
-
-use_bpm 120
-
-#       ;;;  it is a gift to be simple
-#       (phrase [ 1    1   1  1/2 1/2 1/2 1/2]
-#               [-sol -sol do do  re  mi  do])
-
-play 67
-sleep 1
-play 67
-sleep 1
-play 72
-sleep 0.6
-play 72
-sleep 0.4
-play 74
-sleep 0.5
-play 76
-sleep 0.5
-play 72
-sleep 0.6
-
-#       ;;; 'Tis a  gift to be free
-#       (then (phrase [1/2 1/2  1   1/2 1/2   1]
-#                     [mi  fa   sol sol sol   mi]))
-play 76
-sleep 0.4
-play 77
-sleep 0.5
-play 79
-sleep 1
-play 79
-sleep 0.5
-play 79
-sleep 0.5
-play 76
-sleep 1
-
-#       ;;; 'Tis a gift to come round where we ought to be
-#       (then (phrase [1/2 1/2  1   1   1   1   1/2 1/2 1/2 1/2   1]
-#                     [re  do   re  re  re  re  re  mi  re  -si  -sol]))
-
-play 74
-sleep 0.5
-play 72
-sleep 0.5
-play 74
-sleep 1
-play 74
-sleep 1
-play 74
-sleep 1
-play 74
-sleep 1
-play 74
-sleep 0.5
-play 76
-sleep 0.5
-play 74
-sleep 0.5
-play 71
-sleep 0.5
-play 67
-sleep 1
-
-#       ;;; and when we find ourselves in the place just right
-#       (then (phrase [  1   1/2 1/2  1/2 1/2  1  1/2 1/2  1   1]
-#                     [ -sol do  do  re   mi   fa mi  fa   sol]))
-
-#       ;;; it will be in the valley of love and delight
-#       (then (phrase [3/2 1/2   1  1/2 1/2  1  1/2 1/2  1   1/2 1/2  2]
-#                     [fa  sol  mi re  do   re re  re   re  mi  re  si so]))
-
-#       ;;; when true simplicity is gained
-#       (then (phrase [  2 3/2 1/2 1/2 1/2 1/2 1/2 3/2]
-#                     [  4   2   1   2   3   2   1   0]))
-
-#       ;;; to bow and to bend we shan't be ashamed
-#       (then (phrase [1/2   1 1/2 1/2   1   1   1 1/2 1/2 3/2]
-#                     [  1   2   2   3   4   2   1   1   2   1]))
-
-#       ;;; to turn, turn will be our delight
-#       (then (phrase [1/2   2 3/2 1/2   1 1/2 1/2   1]
-#                     [ -3   0   0   1   2   2   3   4]))
-
-#       ;;; till by turning, turning we come round right
-#       (then (phrase [1/2 1/2   1   1   1 1/2 1/2   1   1   1]
-#                     [  4   4   1   1   2   2   1   0   0   0]))

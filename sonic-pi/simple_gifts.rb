0b7d4857ecbd8aa4b93bf25e0b59b0b022bdf9bc
Nada Amin <namin@alum.mit.edu>
2018-03-07 07:04:27 +0000

    finally recognizable @samaaron

--- sonic-pi/simple_gifts.rb
+++ sonic-pi/simple_gifts.rb
@@ -1,6 +1,116 @@
 # Recited from
 # https://www.youtube.com/watch?v=CLAnuG1340g
 
+use_bpm 120
+
+__sol = 67
+__si  = 71
+_do   = 72
+_re   = 74
+_mi   = 76
+_fa   = 77
+_sol  = 79
+
+play_pattern_timed [
+  __sol, #'Tis
+  __sol, #a
+  _do,   #gift
+  _do,
+  _re,
+  _mi,
+  _do,
+  _mi,
+  _fa,
+  _sol,
+  _sol,
+  _sol,
+  _mi,
+  _re,
+  _do,
+  _re,
+  _re,
+  _re,
+  _re,
+  _re,
+  _mi,
+  _re,
+  __si,
+  _sol,
+  _sol,
+  _do,
+  _do,
+  _re,
+  _mi,
+  _do,
+  _mi,
+  _fa,
+  _sol,
+  _sol,
+  _sol,
+  _mi,
+  _re,
+  _do,
+  _re,
+  _re,
+  _mi,
+  _re,
+  _do,
+  __si,
+  _do,
+
+  _sol,
+  _mi,
+  _re,
+  _mi,
+  _fa,
+  _mi,
+  _re,
+  _do,
+
+  _sol,
+  _mi,
+  _re,
+  _mi,
+  _fa,
+  _mi,
+  _re,
+  _do,
+  __sol,
+  _do,
+  _do,
+  _re,
+  _mi,
+  _re,
+  _re,
+  _re,
+  _mi,
+  _re,
+  __sol,
+  _do,
+  _do,
+  _re,
+  _mi,
+  _do,
+  _mi,
+  _fa,
+  _sol,
+  _sol,
+  _fa,
+  _mi,
+  _re,
+  _do,
+  _re,
+  _re,
+  _mi,
+  _re,
+  _do,
+  __si,
+  _do
+], [1]
+
+# Recited from
+# https://www.youtube.com/watch?v=CLAnuG1340g
+
 use_bpm 70
 
 __sol = 67
@@ -68,122 +178,17 @@ play_pattern_timed [
   _do
 ], [1]
 
-# 'Tis  a   gift to  be  simple,
-# __sol do  do   re  mi  do
-# 1     1   1    1   1   1
-
 # 'Tis a gift to be simple,
-# 'Tis
-play __sol
-sleep 0.5
-# a
-play __sol
-sleep 0.5
-# gift
-play _do
-sleep 1
-# to
-play _do
-sleep 0.5
-# be
-play _re
-sleep 0.5
-# sim-
-play _mi
-sleep 0.5
-# -ple
-play _do
-sleep 0.5
-
 
 # 'Tis a gift to be free.
-# 'Tis
-play _mi
-sleep 0.5
-# a
-play _fa
-sleep 0.5
-# gift
-play _sol
-sleep 1
-# to
-play _sol
-sleep 0.5
-# be
-play _sol
-sleep 0.5
-# free
-play _mi
-sleep 1
 
 # 'Tis a gift to come down
-# 'Tis
-play _re
-sleep 0.5
-# a
-play _do
-sleep 0.5
-# gift
-play _re
-sleep 1
-# to
-play _re
-sleep 0.5
-# come
-play _re
-sleep 0.5
-# down
-play _re
-sleep 0.5
 
 # Where we ought to be;
-# Where
-play _re
-sleep 1
-# we
-play _mi
-sleep 0.5
-# ought
-play _re
-sleep 0.5
-# to
-play __si
-sleep 1
-# be
-play __sol
-sleep 1
 
 # And when we find ourselves
-# And
-play _do
-sleep 0.5
-# when
-play _do
-sleep 1
-# we
-play _re
-sleep 0.5
-# find
-play _mi
-sleep 0.5
-# our-
-sleep _do
-sleep 0.5
-# -selves
-sleep _mi
-sleep 0.5
 
 # In the place just right,
-# In
-play _re
-# the
-play _
-# place
-play _sol
-sleep 1
-# just
-play _sol
-# right
 
 # 'Twill be in the value
 

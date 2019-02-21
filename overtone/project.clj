8c7991262abb12ffc1bd9daff708ba66fb99a83d
Nada Amin <na482@cl.cam.ac.uk>
2019-02-21 15:24:18 +0000

    update instructions

--- overtone/project.clj
+++ overtone/project.clj
@@ -1,5 +1,5 @@
 ;; add to ~/.lein/profiles.clj the following line
-;; {:user {:plugins [[cider/cider-nrepl "0.8.1"]]}}}}
+;; {:user {:plugins [[cider/cider-nrepl "0.21.1"]]}}}}
 
 (defproject metasolfeggio "0.1-SNAPSHOT"
   :description "Computer-Aided Harmony and Counterpoint"

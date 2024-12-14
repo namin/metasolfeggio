f20745cb932c4a35ed1aa16d8f9227c048c3902d
Nada Amin <namin@seas.harvard.edu>
2024-12-14 12:00:55 +0100

    revival

---
+++ overtone-current/project.clj
@@ -0,0 +1,14 @@
+;; add to ~/.lein/profiles.clj the following line
+;; {:user {:plugins [[cider/cider-nrepl "0.21.1"]]}}}}
+
+(defproject metasolfeggio "0.2-SNAPSHOT"
+  :description "Computer-Aided Harmony and Counterpoint"
+  :dependencies [;;[org.clojure/clojure "1.8.0"]
+                 ;;[org.clojure/core.logic "0.8.11"]
+                 ;;[org.clojure/core.match "0.3.0-alpha4"]
+                 ;;[loco "0.3.1"]
+                 ;;[overtone "0.10.3"]
+                 [overtone/overtone "0.16.3331"]
+                 ;; cannot upgrade leipzig without changing code?
+                 ;; [leipzig "0.8.1" :exclusions [org.clojure/clojure]]
+                 ])

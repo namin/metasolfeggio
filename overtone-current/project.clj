ca7daa0c950a6bdeafcc43afc2b7009e58341583
Nada Amin <namin@seas.harvard.edu>
2024-12-18 13:44:45 +0100

    revived

--- overtone-current/project.clj
+++ overtone-current/project.clj
@@ -1,5 +1,6 @@
 (defproject your-project-name "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.11.0"]
-                 [overtone "0.16.3331"]]
-  :native-path "native"
-  :source-paths ["src"])
+  :description "Your project description"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [overtone "0.16.3331" :exclusions [net.java.dev.jna/jna]]
+                 [net.java.dev.jna/jna "5.13.0"]]
+  )
---
+++ overtone-current/shakers.clj
@@ -0,0 +1,39 @@
+(ns metasolfeggio.shakers
+  (:use [overtone.live]
+        [overtone.inst sampled-piano]))
+
+(defn player
+  [t speed notes]
+  (when notes
+    (let [n     (first (first notes))
+          s     (second (first notes))
+          notes (next notes)
+          t-next (+ t (/ (* 4 speed) s))]
+      (when n
+        (at t
+          (sampled-piano (note n))))
+      (apply-at t-next #'player [t-next speed notes]))))
+
+(def shakers-melody
+  [[nil 4]         [nil 4]         [:G4 4]         [:G4 4]
+   [:C5 4]         [:C5 8] [:D5 8] [:E5 8] [:C5 8] [:E5 8] [:F5 8]
+   [:G5 4]         [:G5 8] [:G5 8] [:E5 4]         [:D5 8] [:C5 8]
+   [:D5 4]         [:D5 4]         [:D5 4]         [:D5 4]
+   [:D5 8] [:E5 8] [:D5 8] [:B4 8] [:G4 4]         [:G4 4]
+   [:C5 8] [:B4 8] [:C5 8] [:D5 8] [:E5 4]         [:D5 8] [:D5 8]
+   [:E5 4]         [:F5 4]         [:G5 3]                 [:G5 8]
+   [:D5 4]         [:D5 8] [:E5 8] [:D5 4]         [:C5 8] [:C5 8]
+   [:D5 4]         [:C5 8] [:B4 8] [:C5 2]
+   [:G5 2]                         [:E5 3]                 [:D5 8]
+   [:E5 8] [:F5 8] [:E5 8] [:D5 8] [:C5 3]                 [:D5 8]
+   [:E5 4]         [:E5 8] [:F5 8] [:G5 4]         [:E5 4]
+   [:D5 4]         [:D5 8] [:E5 8] [:D5 3]                 [:G4 8]
+   [:C5 2]                         [:C5 3]                 [:D5 8]
+   [:E5 4]         [:E5 8] [:F5 8] [:G5 4]         [:G5 8] [:G5 8]
+   [:D5 4]         [:D5 4]         [:E5 4]         [:E5 8] [:D5 8]
+   [:C5 4]         [:C5 4]         [:C5 2]
+])
+
+(comment
+  (player (now) 380 shakers-melody)
+  (stop))
